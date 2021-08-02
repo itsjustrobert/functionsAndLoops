da2538392ed30471aaae09e5026e4d79dac97d78
itsjustrobert <robertlove99999@gmail.com>
2021-08-02 14:49:15 -0500

    exercises

---
+++ app.js
@@ -0,0 +1,71 @@
+function main(){
+alert("you are insane")
+    // exercise 1 
+    // exerciseOne();
+     exerciseTwo();
+
+
+};
+function exerciseOne(){
+// store all numbers in a given range
+    let numbers=[];
+function exe1(num1,num2){
+
+for(let i = 0; i<=num2-num1;i++){
+  // push numbers to array 
+// console.log(i)
+numbers.push(i);
+// if(num1<=num2){
+
+console.log(numbers);
+// }
+// else{
+//     return -1;
+  
+// }
+}
+
+
+}
+exe1(1,20);
+};
+
+// exercise 2
+function exerciseTwo(){
+
+var multiplants=[1,2,3,4,5,6,7,8,9,10];
+var mutipliers=[1,2,3,4,5,6,7,8,9,10];
+const table = [];
+
+function multiply(){
+for(i=1;i<=multiplants.length;i++)
+
+
+
+
+};
+multiply();
+}
+
+
+
+// exercise 3
+function exerciseThree(){
+
+
+}
+//  exerciseThree()
+
+// exercise 4
+function exerciseFour(){
+
+
+}
+
+// exercise 5
+function exerciseFive(){
+
+
+}
+
+main();
